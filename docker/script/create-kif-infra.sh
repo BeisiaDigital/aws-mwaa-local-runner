403ca3cf55b74374c7bbce62b5d9f2026432d3fc
be-ken-murata <murata-ke@beisia.co.jp>
2024-12-09 18:24:51 +0900

    feature: add localstack and kif-infra settings

---
+++ docker/script/create-kif-infra.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# kif-infraリポジトリ管理外のインフラを構築するコマンド
+#
+#######################
+
+ENDPOINT="http://localstack:4566"
+TF="be-s3-terraform-localstack"
+V1_CONF="be-s3-kif-order-dev"
+V2_CONF="be-kif-dag-dev"
+BUCKETS="be-dlk-dev be-mock-sinops be-sinops-export-dev be-iip-import-dev be-iip-export-dev be-bml-export-dev be-bml-import-dev be-sinops-import-dev"
+
+# create terraform backend bucket
+aws s3 mb s3://${TF} --endpoint ${ENDPOINT}
+
+# create order (receive config) bucket (order bucket is not managed kif-infra repository)
+aws s3 mb s3://${V1_CONF} --endpoint ${ENDPOINT}
+aws s3 cp /root/conf/MBBBUN.yaml s3://${V1_CONF}/b_retail_be_mex/Conf/MBBBUN.yaml  --endpoint ${ENDPOINT}
+aws s3 cp /root/conf/SPAL_Z.yaml s3://${V1_CONF}/be_mex_inventory_real/Conf/SPAL_Z.yaml  --endpoint ${ENDPOINT}
+
+# upload v2 conf
+aws s3 mb s3://${V2_CONF} --endpoint ${ENDPOINT}
+aws s3 cp /root/conf s3://${V2_CONF}/conf  --endpoint ${ENDPOINT} --recursive
+
+# other bucket
+for bucket in ${BUCKETS} ; do 
+    aws s3 mb s3://${bucket} --endpoint ${ENDPOINT}
+    aws s3api put-bucket-versioning --bucket ${bucket} --versioning-configuration Status=Enabled --endpoint ${ENDPOINT}
+done
+
+# create test role
+aws iam create-role --role-name kif-prober-local --assume-role-policy-document '{\"Version\": \"2012-10-17\", \"Statement\": [{\"Effect\": \"Allow\", \"Principal\": {\"AWS\": \"*\"}, \"Action\": \"sts:AssumeRole\"}]}' --endpoint ${ENDPOINT}
+aws iam attach-role-policy --role-name kif-prober-local --policy-arn arn:aws:iam::aws:policy/PowerUserAccess --endpoint ${ENDPOINT}
